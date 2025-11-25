35ad2c9badeaa5d9c1fed670c1b305053bd9581a
Wect <2602710174@qq.com>
2025-11-25 13:44:29 +0800

    🔧 build: api添加NestJS+MongoDB

---
+++ apps/web/src/pages/Lists/Lists.tsx
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react';
+import { Button } from 'antd';
+import 'antd/dist/reset.css';
+
+export default function Lists() {
+  return (
+    <div>
+      <Button>添加组件</Button>
+    </div>
+  )
+}
