ae2fe07b28acb62b35be13ce68935fb362814867
Wect <2602710174@qq.com>
2025-11-29 14:30:30 +0800

    ✨ feat: 组件列表中的元素可以拖动

--- apps/web/src/pages/Marker/components/comList/comItem.tsx
+++ apps/web/src/pages/Marker/components/comList/comItem.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import component from '@type/component';
+import { DragSourceMonitor, useDrag } from 'react-dnd';
+import { ItemTypes } from '@type/DnD';
 
 const ComItem: React.FC<component & { itemWidth: number }> = ({ id, text, name, icon, config, itemWidth }) => {
-  console.log(itemWidth);
+
+  const [{ isDragging }, drag]: any = useDrag(
+    {
+      item: { comp: { id }, originalIndex: -1 },
+      type: ItemTypes.COMITEM,
+      end: (i: any, monitor: DragSourceMonitor) => {
+        if (monitor.didDrop()) {
+          i.originalIndex = -1
+        }
+      },
+      collect: (monitor) => ({
+        isDragging: monitor.isDragging(),
+      }),
+    },
+    [],
+  );
+
   return (
-    <div className="com-item" style={{ width: `${itemWidth}px`, height: `${itemWidth}px` }}>
+    <div ref={drag} className="com-item" style={{ width: `${itemWidth}px`, height: `${itemWidth}px` }}>
       <i style={{ backgroundImage: `url(${icon})` }} className="com-item__icon" />
       <span>
         {text}
--- apps/web/src/pages/Marker/components/comList/comItemList.ts
+++ apps/web/src/pages/Marker/components/comList/comItemList.ts
@@ -2,124 +2,124 @@ import component from '@type/component';
 
 // 组件列表 默认组件
 const componentList: component[] = [
-  // {
-  //   id: 1,
-  //   text: '标题文本',
-  //   name: 'titleText',
-  //   icon: 'https://img01.yzcdn.cn/upload_files/2022/06/17/FjAs6eTmbK_4lQRI3GYXu97Fj_B_.png',
-  //   config: [
-  //     {
-  //       label: "标题内容",
-  //       type: "input",
-  //       format: "title",
-  //       value: '标题'
-  //     },
-  //     {
-  //       label: "描述内容",
-  //       type: "textarea",
-  //       format: "content",
-  //       value: '文本'
-  //     },
-  //     {
-  //       label: "显示位置",
-  //       type: "legend-style-line",
-  //       format: "position",
-  //       config: {
-  //         icon: '#icon-alignleft',
-  //         style: {
-  //           justifyContent: 'left',
-  //         },
-  //         tooltip: '居左显示',
-  //       },
-  //       configOptions: [
-  //         {
-  //           icon: '#icon-alignleft',
-  //           style: {
-  //             justifyContent: 'left',
-  //           },
-  //           tooltip: '居左显示',
-  //         },
-  //         {
-  //           icon: '#icon-aligncenter',
-  //           style: {
-  //             justifyContent: 'center',
-  //           },
-  //           tooltip: '居中显示',
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       label: "标题大小",
-  //       type: "legend-style-line",
-  //       format: "title-size",
-  //       config: {
-  //         icon: '#icon-alignleft',
-  //         style: {
-  //           fontSize: '16px',
-  //         },
-  //         tooltip: '大(16)号',
-  //       },
-  //       configOptions: [
-  //         {
-  //           icon: '#icon-font-16',
-  //           style: {
-  //             fontSize: '16px',
-  //           },
-  //           tooltip: '大(16)号',
-  //         },
-  //         {
-  //           icon: '#icon-font-14',
-  //           style: {
-  //             fontSize: '14px',
-  //           },
-  //           tooltip: '中(14)号',
-  //         },
-  //         {
-  //           icon: '#icon-font-12',
-  //           style: {
-  //             fontSize: '12px',
-  //           },
-  //           tooltip: '小(12)号',
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       label: "描述大小",
-  //       type: "legend-style-line",
-  //       format: "content-size",
-  //       config: {
-  //         icon: '#icon-font-12',
-  //         style: {
-  //           fontSize: '12px',
-  //         },
-  //         tooltip: '小(12)号',
-  //       },
-  //       configOptions: [
-  //         {
-  //           icon: '#icon-font-16',
-  //           style: {
-  //             fontSize: '16px',
-  //           },
-  //           tooltip: '大(16)号',
-  //         },
-  //         {
-  //           icon: '#icon-font-14',
-  //           style: {
-  //             fontSize: '14px',
-  //           },
-  //           tooltip: '中(14)号',
-  //         },
-  //         {
-  //           icon: '#icon-font-12',
-  //           style: {
-  //             fontSize: '12px',
-  //           },
-  //           tooltip: '小(12)号',
-  //         }
-  //       ]
-  //     },
-  //   ],
-  // },
+  {
+    id: 1,
+    text: '标题文本',
+    name: 'titleText',
+    icon: 'https://img01.yzcdn.cn/upload_files/2022/06/17/FjAs6eTmbK_4lQRI3GYXu97Fj_B_.png',
+    config: [
+      {
+        label: "标题内容",
+        type: "input",
+        format: "title",
+        value: '标题'
+      },
+      {
+        label: "描述内容",
+        type: "textarea",
+        format: "content",
+        value: '文本'
+      },
+      {
+        label: "显示位置",
+        type: "legend-style-line",
+        format: "position",
+        config: {
+          icon: '#icon-alignleft',
+          style: {
+            justifyContent: 'left',
+          },
+          tooltip: '居左显示',
+        },
+        configOptions: [
+          {
+            icon: '#icon-alignleft',
+            style: {
+              justifyContent: 'left',
+            },
+            tooltip: '居左显示',
+          },
+          {
+            icon: '#icon-aligncenter',
+            style: {
+              justifyContent: 'center',
+            },
+            tooltip: '居中显示',
+          }
+        ]
+      },
+      {
+        label: "标题大小",
+        type: "legend-style-line",
+        format: "title-size",
+        config: {
+          icon: '#icon-alignleft',
+          style: {
+            fontSize: '16px',
+          },
+          tooltip: '大(16)号',
+        },
+        configOptions: [
+          {
+            icon: '#icon-font-16',
+            style: {
+              fontSize: '16px',
+            },
+            tooltip: '大(16)号',
+          },
+          {
+            icon: '#icon-font-14',
+            style: {
+              fontSize: '14px',
+            },
+            tooltip: '中(14)号',
+          },
+          {
+            icon: '#icon-font-12',
+            style: {
+              fontSize: '12px',
+            },
+            tooltip: '小(12)号',
+          }
+        ]
+      },
+      {
+        label: "描述大小",
+        type: "legend-style-line",
+        format: "content-size",
+        config: {
+          icon: '#icon-font-12',
+          style: {
+            fontSize: '12px',
+          },
+          tooltip: '小(12)号',
+        },
+        configOptions: [
+          {
+            icon: '#icon-font-16',
+            style: {
+              fontSize: '16px',
+            },
+            tooltip: '大(16)号',
+          },
+          {
+            icon: '#icon-font-14',
+            style: {
+              fontSize: '14px',
+            },
+            tooltip: '中(14)号',
+          },
+          {
+            icon: '#icon-font-12',
+            style: {
+              fontSize: '12px',
+            },
+            tooltip: '小(12)号',
+          }
+        ]
+      },
+    ],
+  },
   {
     id: 2,
     text: '店铺信息',
