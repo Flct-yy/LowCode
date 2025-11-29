3ceb58a02dcd54c6cb6d37848a6dd10b62388e91
Wect <2602710174@qq.com>
2025-11-29 09:36:33 +0800

    🔧 build: 安装后端框架 删除内部.git文件夹

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    // ConfigModule.forRoot(),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: process.env.DB_HOST || 'localhost',
+    //   port: parseInt(process.env.DB_PORT || '', 10) || 5432,
+    //   username: process.env.DB_USERNAME || 'postgres',
+    //   password: process.env.DB_PASSWORD || 'password',
+    //   database: process.env.DB_DATABASE || 'lowcode',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
