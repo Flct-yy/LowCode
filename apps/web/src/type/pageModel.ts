8faddda46b8b773914f507c30ac17316b2965cf8
Wect <2602710174@qq.com>
2025-11-30 08:38:46 +0800

    ✨ feat: 修改画布类型

--- apps/web/src/type/pageModel.ts
+++ apps/web/src/type/pageModel.ts
@@ -2,5 +2,8 @@ import { ComponentInstance } from './componentInstance';
 
 export interface PageModel {
   components: ComponentInstance[];
+  showIframe: boolean;
+  compActiveIndex: number;
+  
   // 其他页面级配置，如背景、尺寸等
 }
