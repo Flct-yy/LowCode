0bc8a5be17cc026b1b6d8c73b2a0c3c954e5e620
Wect <2602710174@qq.com>
2026-03-07 11:00:37 +0800

    🦄 refactor(auth): 移除用户认证相关功能及组件

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,18 +3,15 @@ import { useRoutes } from 'react-router-dom';
 import { routes } from '@router/router';
 import '@scss/app.scss';
 import ErrorBoundary from './components/ErrorBoundary';
-import { UserProvider } from './context/UserContext/UserProvider';
 
 function App(): React.ReactNode {
   return (
-    <UserProvider>
-      <ErrorBoundary>
-        <div className="App">
-          {/* 渲染路由组件 */}
-          {useRoutes(routes)}
-        </div>
-      </ErrorBoundary>
-    </UserProvider>
+    <ErrorBoundary>
+      <div className="App">
+        {/* 渲染路由组件 */}
+        {useRoutes(routes)}
+      </div>
+    </ErrorBoundary>
   );
 }
 
--- apps/web/src/router/router.ts
+++ apps/web/src/router/router.ts
@@ -3,36 +3,25 @@ import { RouteObject } from 'react-router-dom';
 import Lists from '@pages/Lists/Lists';
 import Marker from '@pages/Marker/Marker';
 import PreIframe from '@pages/PreIframe/PreIframe';
-import Login from '@pages/Auth/Login';
-import Register from '@pages/Auth/Register';
-import AuthGuard from '@components/AuthGuard';
 
 
 
 const routes: RouteObject[] = [
   {
     path: '/',
-    element: React.createElement(AuthGuard, { children: React.createElement(Lists) }),
+    element: React.createElement(Lists),
   },
   {
     path: '/lists',
-    element: React.createElement(AuthGuard, { children: React.createElement(Lists) }),
+    element: React.createElement(Lists),
   },
   {
     path: '/marker',
-    element: React.createElement(AuthGuard, { children: React.createElement(Marker) }),
+    element: React.createElement(Marker),
   },
   {
     path: '/preview/:pageId',
-    element: React.createElement(AuthGuard, { children: React.createElement(PreIframe) }),
-  },
-  {
-    path: '/auth/login',
-    element: React.createElement(Login),
-  },
-  {
-    path: '/auth/register',
-    element: React.createElement(Register),
+    element: React.createElement(PreIframe),
   },
 ];
 
